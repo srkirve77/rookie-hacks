3bd3e022df11e00f7491ae69ed9a10b59ac3de15
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2020-05-30 23:32:26 -0500

    added listtiles for stores

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'dart:async';
 
 class Home extends StatefulWidget {
   @override
@@ -7,31 +8,71 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List data;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Home'),
         ),
-        body: Center(
+        body: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
           child: FutureBuilder(
               future: DefaultAssetBundle.of(context)
                   .loadString("assets/stores.json"),
               builder: (context, snapshot) {
-                // Decode JSON
+                // HEREEEEEE
                 var myData = json.decode(snapshot.data.toString());
 
                 return new ListView.builder(
                   itemBuilder: (BuildContext context, int index) {
-                    return new Card(
+                    return Card(
                       child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          new Text(myData[index]["storeName"]),
-                          new Text(myData[index]["location"]),
+                          Container(
+                            child: Image.asset((myData[index]["image"]),
+                                fit: BoxFit.cover),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(0),
+                            child: ListTile(
+                              title: Text(myData[index]["storeName"]),
+                              subtitle: Text(myData[index]["location"] +
+                                  ", " +
+                                  myData[index]["hours"]),
+                              trailing: FlatButton(
+                                  color: (myData[index]["isJoined"]
+                                      ? Colors.blue
+                                      : Theme.of(context).primaryColor),
+                                  textColor: Colors.white,
+                                  padding: EdgeInsets.all(6),
+                                  splashColor: Theme.of(context).accentColor,
+                                  child: Text("Join Line",
+                                      style: TextStyle(fontSize: 15)),
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        myData[index]["isJoined"] =
+                                            !myData[index]["isJoined"];
+                                      },
+                                    );
+                                  }),
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(17, 0, 20, 20),
+                                child: Text(
+                                    (myData[index]["currentLine"]).toString() +
+                                        " shoppers in line",
+                                    style: TextStyle(color: Colors.blue)),
+                              ),
+                            ],
+                          )
                         ],
                       ),
+                      color: Colors.grey[5],
                     );
                   },
                   itemCount: myData == null ? 0 : myData.length,
