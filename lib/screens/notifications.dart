5a34dc93aa9030a73d66528601d1054bd8a32f1f
tinamliang <63753268+tinamliang@users.noreply.github.com>
2020-05-30 20:46:30 -0400

    Updated notifications.dart

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,24 +1,26 @@
+import 'package:firebasepushnotifications/widget/messaging_widget.dart';
 import 'package:flutter/material.dart';
 
-class Notifications extends StatefulWidget {
+class Notifications extends StatelessWidget {
+  final String appTitle = 'Qsafe Notification';
   @override
-  _NotificationsState createState() => _NotificationsState();
+  Widget build(BuildContext context) => MaterialApp(
+    title: appTitle,
+    color: Colors.blue,
+    home: MainPage(appTitle: appTitle),
+  );
 }
 
-class _NotificationsState extends State<Notifications> {
+class MainPage extends StatelessWidget {
+  final String appTitle;
+
+  const MainPage({this.appTitle});
+
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text('Notifications'),
-      ),
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Text("Notifications Page"),
-        ],
-      ),
-    ));
-  }
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text(appTitle),
+    ),
+    body: MessagingWidget(),
+  );
 }
